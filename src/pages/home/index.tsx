c4edc080b1f5d5d6006a4d4be532f9adaa9f4d70
renan.garcia <renan.garcia@gazin.com.br>
2022-12-16 17:01:21 -0300

    teste

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,7 +11,7 @@ const Home = () => {
   useLayoutEffect(() => {
       console.log('aaaaaa')
       console.log(user)
-    navigate(user.tipo_usuario === UsersTypes.STUDENT ? '/follow-up/0' : '/reports/student')
+    navigate('/follow-up/0')
   }, [user])
   
   return <></>
