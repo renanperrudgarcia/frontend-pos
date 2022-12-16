42767d2d232b26ac7037e724e533d2a0fc02b82b
renan.garcia <renan.garcia@gazin.com.br>
2022-12-16 12:08:04 -0300

    teste redirect

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -34,7 +34,7 @@ export function Login() {
       console.log({ data, userMe })
       if (meStatus === 200)
         signin({ ...data, ...userMe }, () => {
-          navigate("/");
+            window.location.href = "http://www.devmedia.com.br";
         })
 
       return
